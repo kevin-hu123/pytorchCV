49bc4541fcbae41fcba4622e132ec97d0fe7c498
kevin_hu123 <kevin_hu123@163.com>
2020-09-19 20:13:29 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+# import numpy as np
+# import torch
+# import torchvision
+# import imageio
+
+# z = torch.rand(5,3)
+# x = torch.empty(5,3)
+# a = torch.zeros(5, 3,dtype=torch.long)
+# a = a.new_ones(5,3,dtype=torch.double)
+# print(a)
